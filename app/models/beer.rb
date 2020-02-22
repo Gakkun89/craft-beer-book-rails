3d8dda5e0b0dae6bf8d442194cbd397fee0bb9e9
Mike Warren <mikeywarren89@gmail.com>
2020-02-22 21:09:53 +0900

    added active storage on cloudinary to add user photos to beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,4 +2,5 @@ class Beer < ApplicationRecord
   has_many :beer_hops, dependent: :destroy
   has_many :hops, through: :beer_hops
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo
 end
